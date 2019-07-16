c69bbcd7a7f4fecc8dbb772b01ff95d8f48c0ed4
Marc Fuentes Montenegro <marc.fuentes@valudio.com>
2019-07-16 14:40:31 +0200

    Update Electron version

--- playbook/main.js
+++ playbook/main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: true }})
   mainWindow.loadURL('file://' + __dirname + '/playbook.html')
   mainWindow.on('closed', () => {
     mainWindow = null
